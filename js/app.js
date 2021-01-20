29feee981822d9793429034239e3b423e7112f36
Raghad <raghadmustafa96@yahoo.com>
2021-01-20 12:52:36 +0200

    fixed code lab11 based on comments

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ var firstImageProduct = document.getElementById('first-image');
 var secondImageProduct = document.getElementById('second-image');
 var thirdImageProduct = document.getElementById('third-image');
 var resultsList = document.getElementById('results-list');
+var ViewResultsbutton = document.getElementById('ViewResults');
 
 var firstImageIndex;
 var secondImageIndex;
@@ -16,37 +17,37 @@ var data2 = [];
 var data3 = [];
 var data4 = [];
 
-function Product(productName, pathImage){
+function Product(productName, pathImage ,timeShown ,votes){
   this.productName = productName;
   this.pathImage = pathImage;
-  this.timeShown = 0;
-  this.votes = 0;
+  this.timeShown = timeShown;
+  this.votes = votes;
 
   Product.prototype.allProduct.push(this);
 }
 
 Product.prototype.allProduct = [];
 
-new Product('bag','assets/bag.jpg');
-new Product('banana','assets/banana.jpg');
-new Product('bathroom','assets/bathroom.jpg');
-new Product('boots','assets/boots.jpg');
-new Product('breakfast','assets/breakfast.jpg');
-new Product('bubblegum','assets/bubblegum.jpg');
-new Product('chair','assets/chair.jpg');
-new Product('cthulhu','assets/cthulhu.jpg');
-new Product('dog-duck','assets/dog-duck.jpg');
-new Product('dragon','assets/dragon.jpg');
-new Product('pen','assets/pen.jpg');
-new Product('pet-sweep','assets/pet-sweep.jpg');
-new Product('scissors','assets/scissors.jpg');
-new Product('shark','assets/shark.jpg');
-new Product('sweep','assets/sweep.png');
-new Product('tauntaun','assets/tauntaun.jpg');
-new Product('unicorn','assets/unicorn.jpg');
-new Product('usb','assets/usb.gif');
-new Product('water-can','assets/water-can.jpg');
-new Product('wine-glass','assets/wine-glass.jpg');
+new Product('bag','assets/bag.jpg',0,0);
+new Product('banana','assets/banana.jpg',0,0);
+new Product('bathroom','assets/bathroom.jpg',0,0);
+new Product('boots','assets/boots.jpg',0,0);
+new Product('breakfast','assets/breakfast.jpg',0,0);
+new Product('bubblegum','assets/bubblegum.jpg',0,0);
+new Product('chair','assets/chair.jpg',0,0);
+new Product('cthulhu','assets/cthulhu.jpg',0,0);
+new Product('dog-duck','assets/dog-duck.jpg',0,0);
+new Product('dragon','assets/dragon.jpg',0,0);
+new Product('pen','assets/pen.jpg',0,0);
+new Product('pet-sweep','assets/pet-sweep.jpg',0,0);
+new Product('scissors','assets/scissors.jpg',0,0);
+new Product('shark','assets/shark.jpg',0,0);
+new Product('sweep','assets/sweep.png',0,0);
+new Product('tauntaun','assets/tauntaun.jpg',0,0);
+new Product('unicorn','assets/unicorn.jpg',0,0);
+new Product('usb','assets/usb.gif',0,0);
+new Product('water-can','assets/water-can.jpg',0,0);
+new Product('wine-glass','assets/wine-glass.jpg',0,0);
 
 console.log(Product.prototype.allProduct);
 
@@ -54,6 +55,8 @@ console.log(Product.prototype.allProduct);
 firstImageProduct.addEventListener('click',handleUserClick);
 secondImageProduct.addEventListener('click',handleUserClick);
 thirdImageProduct.addEventListener('click',handleUserClick);
+
+ViewResultsbutton.addEventListener('click',ViewResultsFunction);
 
 function handleUserClick(event){
   attemptsCounter++;
@@ -83,19 +86,11 @@ function handleUserClick(event){
 
   }
   else {
-    var goatResult;
-    for(var i = 0; i < Product.prototype.allProduct.length; i++){
-      // goatResult = document.createElement('li');
-      // goatResult.textContent = Product.prototype.allProduct[i].productName + '--> '+'___' +Product.prototype.allProduct[i].votes + ' votes' + ' ____________  ' + Product.prototype.allProduct[i].timeShown + '  ' + 'times Shown'+'__________________' + customerInterestPrecentage( Product.prototype.allProduct[i].votes , Product.prototype.allProduct[i].timeShown )+ '  %   customer Interest Precentage';
-      data2.push(Product.prototype.allProduct[i].votes);
-      data3.push(Product.prototype.allProduct[i].timeShown);
-      data4.push(customerInterestPrecentage( Product.prototype.allProduct[i].votes , Product.prototype.allProduct[i].timeShown ));
 
-      // resultsList.appendChild(goatResult);
-    }
     firstImageProduct.removeEventListener('click',handleUserClick);
     secondImageProduct.removeEventListener('click',handleUserClick);
     thirdImageProduct.removeEventListener('click',handleUserClick);
+    ViewResultsbutton.disabled = false;
   }
 }
 
@@ -144,7 +139,7 @@ numberOfRoundsForm.addEventListener('submit',numberOfRoundsFunction);
 
 function numberOfRoundsFunction(event){
   event.preventDefault();
-  // resultsList.textContent='';
+  resultsList.textContent='';
   attemptsCounter = 0;
   attemptsAllowed = Number (event.target.numberOfRounds.value);
   console.log(attemptsAllowed);
@@ -157,130 +152,30 @@ function numberOfRoundsFunction(event){
   if(attemptsAllowed <= 0){
     alert('you should fill valid data');
   } else{
-    Product.prototype.allProduct = [];
-    new Product('bag','assets/bag.jpg');
-    new Product('banana','assets/banana.jpg');
-    new Product('bathroom','assets/bathroom.jpg');
-    new Product('boots','assets/boots.jpg');
-    new Product('breakfast','assets/breakfast.jpg');
-    new Product('bubblegum','assets/bubblegum.jpg');
-    new Product('chair','assets/chair.jpg');
-    new Product('cthulhu','assets/cthulhu.jpg');
-    new Product('dog-duck','assets/dog-duck.jpg');
-    new Product('dragon','assets/dragon.jpg');
-    new Product('pen','assets/pen.jpg');
-    new Product('pet-sweep','assets/pet-sweep.jpg');
-    new Product('scissors','assets/scissors.jpg');
-    new Product('shark','assets/shark.jpg');
-    new Product('sweep','assets/sweep.png');
-    new Product('tauntaun','assets/tauntaun.jpg');
-    new Product('unicorn','assets/unicorn.jpg');
-    new Product('usb','assets/usb.gif');
-    new Product('water-can','assets/water-can.jpg');
-    new Product('wine-glass','assets/wine-glass.jpg');
+
+    for(var i = 0; i < Product.prototype.allProduct.length; i++){
+      Product.prototype.allProduct[i].timeShown = 0;
+      Product.prototype.allProduct[i].votes = 0;
+    }
+    console.log( Product.prototype.allProduct);
 
     firstImageProduct.addEventListener('click',handleUserClick);
     secondImageProduct.addEventListener('click',handleUserClick);
     thirdImageProduct.addEventListener('click',handleUserClick);
     renderThreeRandomImages();
-    var myChart2 = document.getElementById('myChart2').getContext('2d');
-
-    var chart2 = new Chart(myChart2, {
-      type: 'bar',
-      data: {
-        labels: labels2,
-        datasets: [ {
-          data: data2,
-          label: 'Number of votes',
-          backgroundColor: colors2,
-        },{
-          data: data3,
-          label: 'times Shown',
-          backgroundColor: colors3
-        },
-        ]
-      },
-      options: {
-        title: {
-          text: 'The Number of votes and times Shown of product',
-          display: true
-        },
-      }
-    });
-
-
-    var myChart4 = document.getElementById('myChart4').getContext('2d');
-
-    var chart4= new Chart(myChart4, {
-      type: 'bar',
-      data: {
-        labels: labels2,
-        datasets: [ {
-          data: data4,
-          label: 'customer Interest Precentage',
-          backgroundColor: colors4
-        }]
-      },
-      options: {
-        title: {
-          text: 'customer Interest Precentage',
-          display: true
-        },
-      }
-    });
   }
 }
 
-// show charts
+function ViewResultsFunction(){
+  resultsList.textContent='';
 
-var labels2 = ['bag', 'banana', 'bathroom', 'boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon', 'pen' ,'pet-sweep' , 'scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'];
-var colors2 = ['#965d62', '#965d62', '#965d62', '#965d62','#965d62', '#965d62', '#965d62', '#965d62','#965d62', '#965d62', '#965d62', '#965d62','#965d62', '#965d62', '#965d62', '#965d62','#965d62', '#965d62', '#965d62', '#965d62'];
-var colors3 = ['#f2d974', '#f2d974', '#f2d974', '#f2d974','#f2d974', '#f2d974', '#f2d974', '#f2d974','#f2d974', '#f2d974', '#f2d974', '#f2d974','#f2d974', '#f2d974', '#f2d974', '#f2d974','#f2d974', '#f2d974', '#f2d974', '#f2d974'];
-var colors4 = ['#534e52', '#534e52', '#534e52', '#534e52','#534e52', '#534e52', '#534e52', '#534e52','#534e52', '#534e52', '#534e52', '#534e52','#534e52', '#534e52', '#534e52', '#534e52','#534e52', '#534e52', '#534e52', '#534e52'];
-
-
-var myChart2 = document.getElementById('myChart2').getContext('2d');
-
-var chart2 = new Chart(myChart2, {
-  type: 'bar',
-  data: {
-    labels: labels2,
-    datasets: [ {
-      data: data2,
-      label: 'Number of votes',
-      backgroundColor: colors2,
-    },{
-      data: data3,
-      label: 'times Shown',
-      backgroundColor: colors3
-    },
-    ]
-  },
-  options: {
-    title: {
-      text: 'The Number of votes and times Shown of product',
-      display: true
-    },
+  var productResult;
+  for(var i = 0; i < Product.prototype.allProduct.length; i++){
+    productResult = document.createElement('li');
+    productResult.textContent = Product.prototype.allProduct[i].productName + '--> '+'___' +Product.prototype.allProduct[i].votes + ' votes' + ' ____________  ' + Product.prototype.allProduct[i].timeShown + '  ' + 'times Shown'+'__________________' + customerInterestPrecentage( Product.prototype.allProduct[i].votes , Product.prototype.allProduct[i].timeShown )+ '  %   customer Interest Precentage';
+    data2.push(Product.prototype.allProduct[i].votes);
+    data3.push(Product.prototype.allProduct[i].timeShown);
+    data4.push(customerInterestPrecentage( Product.prototype.allProduct[i].votes , Product.prototype.allProduct[i].timeShown ));
+    resultsList.appendChild(productResult);
   }
-});
-
-
-var myChart4 = document.getElementById('myChart4').getContext('2d');
-
-var chart4= new Chart(myChart4, {
-  type: 'bar',
-  data: {
-    labels: labels2,
-    datasets: [ {
-      data: data4,
-      label: 'customer Interest Precentage',
-      backgroundColor: colors4
-    }]
-  },
-  options: {
-    title: {
-      text: 'customer Interest Precentage',
-      display: true
-    },
-  }
-});
+}
